1c217dce79cb28609eeb2713eedc76755651e2c0
NiNoX14 <hardstuckd5lul@gmail.com>
2022-06-05 01:44:34 +0300

    feat: hash complete

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,22 @@
+import { createHash } from 'crypto';
+import { existsSync } from 'fs';
+import { readFile } from 'fs/promises';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+const __dirname = dirname(fileURLToPath(import.meta.url));
 export const calculateHash = async () => {
-    // Write your code here 
+    try {
+        const file = join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+        if (existsSync(file)) {
+            const content = await readFile(file, { encoding: 'utf8' });
+            console.log(createHash('sha256').update(content).digest('hex'));
+        }
+        else {
+            throw new Error('FS operation failed');
+        }
+    }
+    catch (e) {
+        console.error(e.message);
+    }
 };
+calculateHash();

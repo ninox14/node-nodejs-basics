c82267623a43b78e479b094b87270d241e7576c9
NiNoX14 <hardstuckd5lul@gmail.com>
2022-06-05 01:46:26 +0300

    feat: zip complete

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,26 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { unlink } from 'fs/promises';
+import { dirname, join } from 'path';
+import { pipeline } from 'stream/promises';
+import { fileURLToPath } from 'url';
+import { createGzip, constants } from 'zlib';
+
+const { Z_BEST_COMPRESSION } = constants;
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 export const compress = async () => {
-    // Write your code here 
+  try {
+    const gzip = createGzip({ level: Z_BEST_COMPRESSION });
+    const sourceFile = join(__dirname, 'files', 'fileToCompress.txt');
+    const destinationFile = join(__dirname, '/files/archieve.gz');
+    const source = createReadStream(sourceFile);
+    const destination = createWriteStream(destinationFile);
+    await pipeline(source, gzip, destination);
+    await unlink(sourceFile);
+    console.log('complete');
+  } catch (e) {
+    console.error(e.message);
+  }
 };
+
+compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,25 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { unlink } from 'fs/promises';
+import { dirname, join } from 'path';
+import { pipeline } from 'stream/promises';
+import { fileURLToPath } from 'url';
+import { createGunzip, gunzip } from 'zlib';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 export const decompress = async () => {
-    // Write your code here 
+  try {
+    const gunzip = createGunzip({ info: true });
+    const sourceFile = join(__dirname, '/files/archieve.gz');
+    const destinationFile = join(__dirname, '/files/fileToCompress.txt');
+    const source = createReadStream(sourceFile);
+    const destination = createWriteStream(destinationFile);
+    await pipeline(source, gunzip, destination);
+    await unlink(sourceFile);
+    console.log('complete');
+  } catch (e) {
+    console.error(e.message);
+  }
 };
+
+decompress();

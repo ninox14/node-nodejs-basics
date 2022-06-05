9fb5d6b7b14aa31a07a0acdbb9d1bfd0f4eafc1f
NiNoX14 <hardstuckd5lul@gmail.com>
2022-06-05 17:14:35 +0300

    feat: child processes done

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,16 @@
+import { fork } from 'child_process';
+import { join, dirname } from 'path';
+import { stderr, stdin, stdout } from 'process';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+  const file = join(__dirname, '/files/script.js');
+
+  fork(file, [...args], { stdio: [stdin, stdout, stderr, 'ipc'] });
 };
+
+const args = process.argv.slice(2);
+
+spawnChildProcess(args);

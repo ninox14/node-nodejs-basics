67b79c32ec53c80d6da40a2e9a43958b7e0567c8
NiNoX14 <hardstuckd5lul@gmail.com>
2022-06-05 17:13:26 +0300

    feat: streams complete

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,17 @@
+import { stdin, stdout } from 'process';
+import { Transform } from 'stream';
+
 export const transform = async () => {
-    // Write your code here 
+  try {
+    const reverse = new Transform({
+      transform(chunk, encoding, callback) {
+        callback(null, `${chunk.toString().split('').reverse().join('')}\n`);
+      },
+    });
+    stdin.pipe(reverse).pipe(stdout);
+  } catch (e) {
+    console.error(e.message);
+  }
 };
+
+transform();

590286489a435958c9ec8827e3f283eceb8d1966
NiNoX14 <hardstuckd5lul@gmail.com>
2022-06-05 18:10:06 +0300

    refactor: add explaining comments

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -7,7 +7,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export const spawnChildProcess = async (args) => {
   const file = join(__dirname, '/files/script.js');
-
+  // LOOK HERE: https://discord.com/channels/755676888680366081/980732948745695373/982962934407901214
   fork(file, [...args], { stdio: [stdin, stdout, stderr, 'ipc'] });
 };
 
